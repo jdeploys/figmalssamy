e1a6fc5ecda5297a45644968e64b40dfd0906347
jskim <dcjuja@gmail.com>
2024-03-20 18:57:34 +0900

    💄 스타일 정리

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -16,18 +16,18 @@ export default function Page() {
   const [value, setValue] = useState<string>('');
 
   function rewriteSentence(sentence: string) {
-    let periodCount = (sentence.match(new RegExp('\\.', "g")) || []).length;
-    let exclamationCount = (sentence.match(new RegExp('\\!', "g")) || []).length;
-    let questionCount = (sentence.match(new RegExp('\\?', "g")) || []).length;
+    let periodCount = (sentence.match(new RegExp('\\.', 'g')) || []).length;
+    let exclamationCount = (sentence.match(new RegExp('\\!', 'g')) || []).length;
+    let questionCount = (sentence.match(new RegExp('\\?', 'g')) || []).length;
 
     if (periodCount + exclamationCount + questionCount === 1) {
-      return sentence.replace(/\.$/, "");
+      return sentence.replace(/\.$/, '');
     } else if (periodCount + exclamationCount + questionCount > 1) {
       let lastCharacter = sentence.slice(-1);
-      if (lastCharacter !== ".") {
-        sentence += ".";
+      if (lastCharacter !== '.') {
+        sentence += '.';
       } else {
-        sentence = sentence.replace(/[.!?]+$/, ".");
+        sentence = sentence.replace(/[.!?]+$/, '.');
       }
       return sentence;
     }
@@ -55,22 +55,28 @@ export default function Page() {
   };
 
   return (
-    <div className="p-6 bg-black h-screen">
-      <div className="flex flex-row items-end gap-4">
-        <textarea
-          className="text-gray-900 p-2 min-w-[400px] min-h-[400px]"
-          onChange={(e) => {
-            setValue(e.target.value);
-          }}
-        />
-        <button className="rounded bg-blue-700 p-3" onClick={checkSpell}>
-          검사하기
-        </button>
+    <div className="p-6 bg-black h-screen text-gray-300">
+      <h1 className="font-bold text-xl">맞춤법 검사기</h1>
+      <div className="flex flex-col gap-2 mt-6">
+        <label>맞춤법 확인할 문장</label>
+        <div className="flex flex-row items-end gap-4">
+          <textarea
+            className="bg-gray-800 p-2 min-w-[400px] min-h-[200px] rounded-md"
+            placeholder="여기에 맞춤법 검사할 문장을 적어주세요"
+            onChange={(e) => {
+              setValue(e.target.value);
+            }}
+          />
+          <button className="rounded bg-blue-700 px-3 py-2" onClick={checkSpell}>
+            검사하기
+          </button>
+        </div>
       </div>
-      <div className="flex flex-col">
+      <div className="flex flex-col gap-2 mt-6">
         <label>결과</label>
         <textarea
-          className="text-gray-900 p-2 min-w-[400px] min-h-[400px]"
+          className="bg-gray-800 p-2 min-w-[400px] min-h-[400px] rounded-md"
+          placeholder="문장을 작성하고 결과를 확인해보세요"
           readOnly
           value={checkSpellResult}
         />
